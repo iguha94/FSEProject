8f50a13f8966cdaec8a678db9d1c51126cd4c178
MBiggs <mikayla-biggs@uiowa.edu>
2020-02-18 18:48:21 -0600

    local host DB connection & template html page for REST

--- Backend/src/REST.py
+++ Backend/src/REST.py
@@ -5,28 +5,21 @@ Created on Tue Feb 18 17:01:14 2020
 @author: MBiggs
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
+from flask_mysqldb import MySQL
+import mysql.connector 
+
+   
+db = mysql.connector.connect(user='root', password='fseTeam4',
+                              host='localhost')                           
+mycursor = db.cursor()
+#mycursor.execute("CREATE DATABASE mydatabase")
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
