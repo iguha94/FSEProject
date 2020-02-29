b3e25c8653c47d5f7604e6640bb80c44b89d734e
Indranil Guha <indranil-guha@uiowa.edu>
2020-02-28 21:26:57 -0600

    Login and Authorization Codes

--- Backend/src/REST.py
+++ Backend/src/REST.py
@@ -6,20 +6,91 @@ Created on Tue Feb 18 17:01:14 2020
 """
 
 from flask import Flask, jsonify, render_template, request
-from flask_mysqldb import MySQL
-import mysql.connector 
+import mysql.connector
+import base64
+import re
 
-   
-db = mysql.connector.connect(user='root', password='fseTeam4',
-                              host='localhost')                           
+db = mysql.connector.connect(user='root', password='',
+                              host='localhost',database='FSETEAM04',
+                              auth_plugin='mysql_native_password')
 mycursor = db.cursor()
-#mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute('USE FSETEAM04')
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
 app = Flask(__name__)
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    if not request.json or not 'Email' in request.json or request.json['Email'] == '':
+        return jsonify({'Message': 'EmailID is Mandatory'}),400
+    if not request.json or not 'PassWord' in request.json or request.json['PassWord'] == '' :
+        return jsonify({'Message': 'PassWord is Mandatory'}),400
+    EmailID = request.json['Email']
+    PassWord = base64.b64encode(request.json['PassWord'].encode("utf-8"))
+    sql = "SELECT * FROM Users WHERE Email=%s AND PassWord=%s"
+    val = (EmailID,PassWord,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    if len(myresult)!=1:
+        return jsonify({'Message': 'Email or Password is Incorrect'}),400
+
+    return jsonify({'Message': 'Logged in Successfully'}), 200
+
+@app.route('/signup',methods=['POST'])
+def signup():
+    if not request.json or not 'Email' in request.json or request.json['Email'] == '' or not re.search(regex, request.json['Email']):
+        return jsonify({'Message': 'Valid EmailID is Mandatory'}),400
+    if not request.json or not 'PassWord' in request.json or request.json['Password'] == '' :
+        return jsonify({'Message': 'PassWord is Mandatory'}),400
+
+    EmailID = request.json['Email']
+    sql = "SELECT * FROM Users WHERE Email=%s"
+    val = (EmailID,)
+    mycursor.execute(sql,val)
+    myresult = mycursor.fetchall()
+    print('Total record in db: ',len(myresult))
+    if len(myresult)>0:
+        return jsonify({'Message': 'User with This EmailID is already Present'}),400
+    msg=""
+    if 'LastName' not in request.json:
+        return jsonify({'Message': 'Last Name is Mandatory'}), 400
+    if 'FirstName' not in request.json:
+        return jsonify({'Message': 'First Name is Mandatory'}), 400
+    if 'Street' not in request.json:
+        return jsonify({'Message': 'Street Name is Mandatory'}), 400
+    if 'Country' not in request.json:
+        return jsonify({'Message': 'Country Name is Mandatory'}), 400
+    if 'ZIP' not in request.json:
+        return jsonify({'Message': 'Zip is Mandatory'}), 400
+    if 'State' not in request.json:
+        return jsonify({'Message': 'State Name is Mandatory'}), 400
+    if 'City' not in request.json:
+        return jsonify({'Message': 'City is Mandatory'}), 400
+    if 'Phone' not in request.json:
+        return jsonify({'Message': 'Phone Number is Mandatory'}), 400
+
+    LastName = request.json['LastName']
+    FirstName = request.json['FirstName']
+    Street = request.json['Street']
+    City = request.json['Country']
+    ZIP = request.json['ZIP']
+    State = request.json['State']
+    Country = request.json['Country']
+    PassWord = base64.b64encode(request.json['PassWord'].encode("utf-8"))
+    Phone = request.json['Phone']
+    Status = 'Default'
+    CurStatus=''
+    sql = "INSERT INTO Users (LastName,FirstName,Street,City,ZIP,State,Country,Email,PassWord,Phone,Status,CurStatus) VALUES ( %s, %s,%s,%s, %s, %s,%s,%s, %s, %s,%s,%s)"
+    val = (LastName,FirstName,Street,City,ZIP,State,Country,EmailID,PassWord,Phone,Status,CurStatus)
+    mycursor.execute(sql, val)
+    db.commit()
+    return jsonify({'Message': 'Account Created Successfully'}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
