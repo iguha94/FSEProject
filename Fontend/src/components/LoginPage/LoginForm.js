73130589ef55eeb1a38bc8f98fb20f8d836a4603
Indranil Guha <indranil-guha@uiowa.edu>
2020-03-13 17:16:41 -0500

    Front end Codes

--- Fontend/src/components/LoginPage/LoginForm.js
+++ Fontend/src/components/LoginPage/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Panel, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
-
+import axios from 'axios'
+//const Url='https://cors-anywhere.herokuapp.com/http://localhost:5000/'
+const Url='http://localhost:5000/'
 
 const divStyle = {
   display: 'flex',
@@ -22,10 +24,44 @@ const buttonStyle = {
 
 class LoginForm extends Component {
 
-  handleFormSubmit(e) {
-    e.preventDefault();
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      validated: false,
+      email: '',
+      password: '',
+    };
 
-    console.log("FORM SUBMIT!");
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleEmailChange(e) {
+   this.setState({email: e.target.value});
+  }
+  handlePasswordChange(e) {
+   this.setState({password: e.target.value});
+  }
+  handleFormSubmit(e) {
+     e.preventDefault();
+     console.log('In handle submit')
+      const loginurl=Url+'login'
+      console.log('Email Id: '+this.state.email)
+
+    const payload = {
+	Email:this.state.email,
+	PassWord: this.state.password
+    }
+    axios({method:'post',
+          url: loginurl,
+          data:{
+            payload
+    }
+  }).then(data=>console.log(data))
+        .catch(err=>console.log(err))
+
+    console.log("FORM SUBMITTED!");
 
   }
 
@@ -35,10 +71,10 @@ class LoginForm extends Component {
         <Panel style={panelStyle}>
           <Form horizontal className="LoginForm" id="loginForm">
             <FormGroup controlId="formEmail">
-              <FormControl type="email" placeholder="Email Address" />
+              <FormControl type="email" value={this.state.email} onChange={this.handleEmailChange} placeholder="Email Address" />
             </FormGroup>
             <FormGroup controlId="formPassword">
-              <FormControl type="password" placeholder="Password" />
+              <FormControl type="password" value={this.state.password} onChange={this.handlePasswordChange} placeholder="Password" />
             </FormGroup>
             <FormGroup style={buttonStyle} controlId="formSubmit">
               <Button bsStyle="primary" type="submit" onClick={this.handleFormSubmit}>
@@ -53,3 +89,4 @@ class LoginForm extends Component {
 }
 
 export default LoginForm;
+
