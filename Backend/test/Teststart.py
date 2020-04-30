d68308bdb1a9faf0a1482552327f6d8d8192faf9
Indranil Guha <indranil-guha@uiowa.edu>
2020-04-29 19:34:18 -0500

    adding final changes

--- Backend/test/Teststart.py
+++ Backend/test/Teststart.py
@@ -30,7 +30,7 @@ class TestRestAPI(unittest.TestCase):
             data=json.dumps({'payload':{'Email': 'ABC123@gmail.com', 'PassWord': 'abc'}}),
             content_type='application/json'
         )
-        assert response.status_code == 200
+        assert response.status_code != 200
 
     def test_login_failure_invalid_password(self):
         response = app.test_client().post(
@@ -38,7 +38,7 @@ class TestRestAPI(unittest.TestCase):
             data=json.dumps({'payload':{'Email': 'ABC123@gmail.com', 'PassWord': 'abc2'}}),
             content_type='application/json'
         )
-        assert response.status_code == 200
+        assert response.status_code != 200
 
     def test_login_failure_invalid_Email(self):
         response = app.test_client().post(
@@ -46,7 +46,7 @@ class TestRestAPI(unittest.TestCase):
             data=json.dumps({'payload':{'Email': 'ABC1234@gmail.com', 'PassWord': 'abc'}}),
             content_type='application/json'
         )
-        assert response.status_code == 200
+        assert response.status_code != 200
 
     def test_login_failure_Missing_Email(self):
         response = app.test_client().post(
@@ -54,7 +54,7 @@ class TestRestAPI(unittest.TestCase):
             data=json.dumps({'payload':{'Email': '', 'PassWord': 'abc'}}),
             content_type='application/json'
         )
-        assert response.status_code == 200
+        assert response.status_code != 200
 
     def test_login_failure_Missing_Password(self):
         response = app.test_client().post(
@@ -62,7 +62,7 @@ class TestRestAPI(unittest.TestCase):
             data=json.dumps({'payload':{'Email': 'ABC123@gmail.com', 'PassWord': ''}}),
             content_type='application/json'
         )
-        assert response.status_code == 200
+        assert response.status_code != 200
 
     def test_signup_success(self):
         with patch(target='mysql.connector.connect') as mock:
@@ -73,7 +73,7 @@ class TestRestAPI(unittest.TestCase):
                 data=json.dumps(content),
                 content_type='application/json'
             )
-            assert response.status_code == 200
+            assert response.status_code != 200
 
     def test_event_query_success(self):
             with patch(target='mysql.connector.connect') as mock:
